3761400340c1a05de52f2038a5b8870ce2de5a09
Bianca Oura <biancaoura@gmail.com>
2022-10-06 01:02:22 -0300

    altera testes

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,29 +1,30 @@
-import { cleanup, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './helpers/renderwithRouter';
 
-describe('Login page test', () => {
-  const EMAIL_TESTID = 'email-input';
-  const PASSWORD_TESTID = 'password-input';
-  const BUTTON_TESTID = 'login-submit-btn';
+const EMAIL_TESTID = 'email-input';
+const PASSWORD_TESTID = 'password-input';
+const LOGIN_BTN = 'login-submit-btn';
 
+describe('Login page test', () => {
+  let renderHistory;
   beforeEach(() => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    renderHistory = history;
   });
 
-  afterEach(() => cleanup());
-
-  test('Should have two inputs and a button', () => {
+  it('1 - Should have two inputs and a button', () => {
     expect(screen.getByTestId(EMAIL_TESTID)).toBeDefined();
     expect(screen.getByTestId(PASSWORD_TESTID)).toBeDefined();
-    expect(screen.getByTestId(BUTTON_TESTID)).toBeDefined();
+    expect(screen.getByTestId(LOGIN_BTN)).toBeDefined();
   });
-  test('The button functionality', () => {
+
+  it('2 - Button should function properly', () => {
     userEvent.type(screen.getByTestId(EMAIL_TESTID), 'email@email');
     userEvent.type(screen.getByTestId(PASSWORD_TESTID), '1234567');
 
-    expect(screen.getByTestId(BUTTON_TESTID)).toBeDisabled();
+    expect(screen.getByTestId(LOGIN_BTN)).toBeDisabled();
 
     userEvent.clear(screen.getByTestId(EMAIL_TESTID));
     userEvent.clear(screen.getByTestId(PASSWORD_TESTID));
@@ -31,10 +32,10 @@ describe('Login page test', () => {
     userEvent.type(screen.getByTestId(EMAIL_TESTID), 'email@email.com');
     userEvent.type(screen.getByTestId(PASSWORD_TESTID), '1234567');
 
-    expect(screen.getByTestId(BUTTON_TESTID)).toBeEnabled();
+    expect(screen.getByTestId(LOGIN_BTN)).toBeEnabled();
 
-    userEvent.click(screen.getByTestId(BUTTON_TESTID));
+    userEvent.click(screen.getByTestId(LOGIN_BTN));
 
-    expect(screen.getByText(/Meals/i)).toBeDefined();
+    expect(renderHistory.location.pathname).toBe('/meals');
   });
 });
