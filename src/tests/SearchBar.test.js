fda7c7e165665d61e4795b90f57678d385933e11
Bianca Oura <biancaoura@gmail.com>
2022-10-06 00:13:46 -0300

    altera testes

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,19 +1,17 @@
 import { cleanup, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import drinkCategories from '../../cypress/mocks/drinkCategories';
-import mealCategories from '../../cypress/mocks/mealCategories';
-import meals from '../../cypress/mocks/meals';
-import oneDrink from '../../cypress/mocks/oneDrink';
-import oneMeal from '../../cypress/mocks/oneMeal';
+import drinkCategories from './helpers/drinkCategories';
+import mealCategories from './helpers/mealCategories';
+import mockMeals from './helpers/mockMeals';
+import oneDrink from './helpers/oneDrink';
+import oneMeal from './helpers/oneMeal';
 import App from '../App';
 import drinksSearchedY from '../helpers/mockDrinksY';
 import mealsByLetter from '../helpers/mockMealsByLetter';
 import drinks from './helpers/mockDrinks';
 import renderWithRouter from './helpers/renderwithRouter';
-import mealsByIngredient from '../../cypress/mocks/mealsByIngredient';
-import drinksByIngredient from '../../cypress/mocks/drinksByIngredient';
-import emptyMeals from '../../cypress/mocks/emptyMeals';
-import emptyDrinks from '../../cypress/mocks/emptyDrinks';
+import mealsByIngredient from './helpers/mealsByIngredient';
+import drinksByIngredient from './helpers/drinksByIngredient';
 
 const INGREDIENT_DATA_TESTID = 'ingredient-search-radio';
 const RECIPE_NAME_DATA_TESTID = 'name-search-radio';
@@ -23,13 +21,26 @@ const SEARCH_BUTTON_DATA_TESTID = 'exec-search-btn';
 const SEARCH_ICON_IMAGE = 'search-top-btn';
 const CARD_NAME = '0-card-name';
 
-describe('SearchBar page tests', () => {
+const mockPromise = (type, typeCat, searchInput) => jest.spyOn(global, 'fetch')
+  .mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(type),
+  })
+  .mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(typeCat),
+  })
+  .mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(searchInput),
+  });
+
+describe('Testing SearchBar component', () => {
+  beforeEach(() => jest.clearAllMocks());
+
   afterEach(() => {
     cleanup();
     jest.restoreAllMocks();
   });
 
-  test('If all elements are in the component', () => {
+  it('1 - Should render the correct elements', () => {
     renderWithRouter(<App />, '/meals');
 
     const searchImgButton = screen.getByTestId(SEARCH_ICON_IMAGE);
@@ -51,35 +62,13 @@ describe('SearchBar page tests', () => {
 
     elements.forEach((el) => expect(el).toBeInTheDocument());
   });
-  test('If when an radio input is selected, the others inputs are not selected', () => {
-    renderWithRouter(<App />, '/meals');
 
-    const searchImgButton = screen.getByTestId(SEARCH_ICON_IMAGE);
-    userEvent.click(searchImgButton);
-
-    const ingredientRadio = screen.getByTestId(INGREDIENT_DATA_TESTID);
-    const recipeNameRadio = screen.getByTestId(RECIPE_NAME_DATA_TESTID);
-    const firstLetterRadio = screen.getByTestId(FIRST_LETTER_DATA_TESTID);
-
-    userEvent.click(recipeNameRadio);
-    expect(recipeNameRadio).toBeChecked();
-    expect(ingredientRadio).not.toBeChecked();
-    expect(firstLetterRadio).not.toBeChecked();
-  });
-  test('In Drinks Page if when typed in search input and select the ingredient radio input, the correct element appears when search button is clicked', async () => {
-    jest.clearAllMocks();
-    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinks),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinkCategories),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinksSearchedY),
-    });
+  it('2 - Should filter the correct recipes in /drinks', async () => {
+    mockPromise(drinks, drinkCategories, drinksSearchedY);
 
     renderWithRouter(<App />, '/drinks');
 
     userEvent.click(screen.getByTestId(SEARCH_ICON_IMAGE));
-
     userEvent.click(screen.getByTestId(FIRST_LETTER_DATA_TESTID));
     userEvent.type(screen.getByTestId(SEARCH_INPUT_DATA_TESTID), 'y');
 
@@ -89,20 +78,13 @@ describe('SearchBar page tests', () => {
 
     expect(await screen.findByTestId(CARD_NAME)).toHaveTextContent('Yellow Bird');
   });
-  test('In Meals Page if when typed in search input and select the ingredient radio input, the correct element appears when search button is clicked', async () => {
-    jest.clearAllMocks();
-    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(meals),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(mealCategories),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(mealsByLetter),
-    });
+
+  it('3 - Should filter the correct recipes in /meals', async () => {
+    mockPromise(mockMeals, mealCategories, mealsByLetter);
 
     renderWithRouter(<App />, '/meals');
 
     userEvent.click(screen.getByTestId(SEARCH_ICON_IMAGE));
-
     userEvent.click(screen.getByTestId(FIRST_LETTER_DATA_TESTID));
     userEvent.type(screen.getByTestId(SEARCH_INPUT_DATA_TESTID), 'd');
 
@@ -112,12 +94,14 @@ describe('SearchBar page tests', () => {
 
     expect(await screen.findByTestId(CARD_NAME)).toHaveTextContent('Dal fry');
   });
-  test('In Drinks page if when first letter radio is selected and typed more than one letter, an alert appears on the screen', () => {
-    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinks),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinkCategories),
-    });
+  it('4 - Should display an alert if user inputs more than 1 letter when searching for the first letter in /drinks', () => {
+    jest.spyOn(global, 'fetch')
+      .mockResolvedValueOnce({
+        json: jest.fn().mockResolvedValue(drinks),
+      })
+      .mockResolvedValueOnce({
+        json: jest.fn().mockResolvedValue(drinkCategories),
+      });
     jest.spyOn(window, 'alert').mockImplementation();
     renderWithRouter(<App />, '/drinks');
 
@@ -131,12 +115,14 @@ describe('SearchBar page tests', () => {
 
     expect(window.alert).toHaveBeenCalled();
   });
-  test('In Meals page if when first letter radio is selected and typed more than one letter, an alert appears on the screen', () => {
-    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(meals),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(mealCategories),
-    });
+  it('5 - Should display an alert if user inputs more than 1 letter when searching for the first letter in /meals', () => {
+    jest.spyOn(global, 'fetch')
+      .mockResolvedValueOnce({
+        json: jest.fn().mockResolvedValue(mockMeals),
+      })
+      .mockResolvedValueOnce({
+        json: jest.fn().mockResolvedValue(mealCategories),
+      });
     jest.spyOn(window, 'alert').mockImplementation();
     renderWithRouter(<App />, '/meals');
 
@@ -150,36 +136,8 @@ describe('SearchBar page tests', () => {
 
     expect(window.alert).toHaveBeenCalled();
   });
-  test('If the user is redirected to the proper recipe details page', async () => {
-    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(meals),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(mealCategories),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(oneMeal),
-    });
-
-    renderWithRouter(<App />, '/meals');
-
-    expect(await screen.findByText('Corba')).toBeDefined();
-
-    userEvent.click(screen.getByTestId(SEARCH_ICON_IMAGE));
-
-    userEvent.click(screen.getByTestId(RECIPE_NAME_DATA_TESTID));
-    userEvent.type(screen.getByTestId(SEARCH_INPUT_DATA_TESTID), 'Arrabiata');
-
-    userEvent.click(screen.getByTestId(SEARCH_BUTTON_DATA_TESTID));
-
-    expect(await screen.findByRole('heading', { name: /spicy arrabiata penne/i, level: 1 }));
-  });
-  test('If the user is redirected to the proper recipe details page', async () => {
-    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinks),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinkCategories),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(oneDrink),
-    });
+  it('6 - Should redirect to the correct drink recipe\'s details page', async () => {
+    mockPromise(drinks, drinkCategories, oneDrink);
 
     renderWithRouter(<App />, '/drinks');
 
@@ -194,90 +152,55 @@ describe('SearchBar page tests', () => {
 
     expect(await screen.findByRole('heading', { name: /Aquamarine/i, level: 1 }));
   });
-  test('If the meals ingredient search', async () => {
-    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(meals),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(mealCategories),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(mealsByIngredient),
-    });
+  it('7 - Should redirect to the correct meal recipe\'s details page', async () => {
+    mockPromise(mockMeals, mealCategories, oneMeal);
 
     renderWithRouter(<App />, '/meals');
 
     expect(await screen.findByText('Corba')).toBeDefined();
 
     userEvent.click(screen.getByTestId(SEARCH_ICON_IMAGE));
+    userEvent.click(screen.getByTestId(RECIPE_NAME_DATA_TESTID));
+    userEvent.type(screen.getByTestId(SEARCH_INPUT_DATA_TESTID), 'Arrabiata');
+    userEvent.click(screen.getByTestId(SEARCH_BUTTON_DATA_TESTID));
 
+    expect(await screen.findByRole('heading', { name: /spicy arrabiata penne/i, level: 1 }));
+  });
+
+  it('8 - Should filter by food ingredient name', async () => {
+    mockPromise(mockMeals, mealCategories, mealsByIngredient);
+
+    renderWithRouter(<App />, '/meals');
+
+    expect(await screen.findByText('Corba')).toBeDefined();
+
+    userEvent.click(screen.getByTestId(SEARCH_ICON_IMAGE));
     userEvent.click(screen.getByTestId(INGREDIENT_DATA_TESTID));
     userEvent.type(screen.getByTestId(SEARCH_INPUT_DATA_TESTID), 'Chicken');
-
     userEvent.click(screen.getByTestId(SEARCH_BUTTON_DATA_TESTID));
 
     expect(await screen.findByTestId(CARD_NAME)).toHaveTextContent('Brown Stew Chicken');
   });
-  test('If the drinks ingredient search', async () => {
-    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinks),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinkCategories),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinksByIngredient),
-    });
+  it('9 - Should filter by drink ingredient name', async () => {
+    mockPromise(drinks, drinkCategories, drinksByIngredient);
 
     renderWithRouter(<App />, '/drinks');
 
     expect(await screen.findByText('GG')).toBeDefined();
 
     userEvent.click(screen.getByTestId(SEARCH_ICON_IMAGE));
-
     userEvent.click(screen.getByTestId(INGREDIENT_DATA_TESTID));
     userEvent.type(screen.getByTestId(SEARCH_INPUT_DATA_TESTID), 'Light rum');
-
     userEvent.click(screen.getByTestId(SEARCH_BUTTON_DATA_TESTID));
 
     expect(await screen.findByTestId(CARD_NAME)).toHaveTextContent('151 Florida Bushwacker');
   });
-  test('If the alert is called', async () => {
-    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinks),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(drinkCategories),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(emptyDrinks),
-    })
-      .mockResolvedValueOnce({
-        json: jest.fn().mockResolvedValue(drinks),
-      });
-
-    jest.spyOn(window, 'alert').mockImplementation();
-
-    renderWithRouter(<App />, '/drinks');
-
-    expect(await screen.findByText('GG')).toBeDefined();
-
-    userEvent.click(screen.getByTestId(SEARCH_ICON_IMAGE));
-
-    userEvent.click(screen.getByTestId(RECIPE_NAME_DATA_TESTID));
-    userEvent.type(screen.getByTestId(SEARCH_INPUT_DATA_TESTID), 'Xablau');
-
-    userEvent.click(screen.getByTestId(SEARCH_BUTTON_DATA_TESTID));
-
-    await waitFor(() => expect(window.alert).toHaveBeenCalled());
-  });
-  test('If the alert is called', async () => {
-    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(meals),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(mealCategories),
-    }).mockResolvedValueOnce({
-      json: jest.fn().mockResolvedValue(emptyMeals),
-    });
+  it('10 - Should display an alert if no recipe is found', async () => {
+    mockPromise(mockMeals, mealCategories, { meals: null });
 
     jest.spyOn(window, 'alert').mockImplementation();
 
     renderWithRouter(<App />, '/meals');
-    console.log(window.alert);
 
     expect(await screen.findByText('Corba')).toBeDefined();
 
