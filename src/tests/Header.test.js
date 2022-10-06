038eb37a1e8436ae9c6287fc148620726f30bb8e
Bianca Oura <biancaoura@gmail.com>
2022-10-06 01:08:38 -0300

    altera testes

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,27 +1,34 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Header from '../components/Header';
+import App from '../App';
 import renderWithRouter from './helpers/renderwithRouter';
 
-describe('Header page tests', () => {
-  it('Should redirect to /profile when profile icon is clicked', () => {
-    renderWithRouter(<Header />);
+describe('Testing Header component', () => {
+  let renderHistory;
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />, '/meals');
+    renderHistory = history;
+  });
+
+  it('1 - Should redirect to /profile when profile icon is clicked', () => {
     const profileBtn = screen.getByTestId('profile-top-btn');
     userEvent.click(profileBtn);
 
-    const profileTitle = screen.getByRole('heading', { level: 1, name: /Profile/i });
-    expect(profileTitle);
+    const { location: { pathname } } = renderHistory;
+    expect(pathname).toBe('/profile');
   });
-  it('Should render the right page titles', () => {
-    const { history } = renderWithRouter(<Header />);
-    history.push('/drinks');
-    const drinksTitle = screen.getByRole('heading', { level: 1, name: /Drinks/i });
-    expect(drinksTitle).toBeInTheDocument();
-    history.push('/done-recipes');
-    const doneRecipesTitle = screen.getByRole('heading', { level: 1, name: /Done Recipes/i });
-    expect(doneRecipesTitle).toBeInTheDocument();
-    history.push('/favorite-recipes');
-    const favoriteRecipesTitle = screen.getByRole('heading', { level: 1, name: /Favorite Recipes/i });
-    expect(favoriteRecipesTitle).toBeInTheDocument();
+
+  it('2 - Should render the right page titles', () => {
+    renderHistory.push('/drinks');
+    const drinkLogo = screen.getByAltText(/drinks/i);
+    expect(drinkLogo).toBeInTheDocument();
+
+    renderHistory.push('/done-recipes');
+    const doneRecipesLogo = screen.getByAltText(/done-recipes/i);
+    expect(doneRecipesLogo).toBeInTheDocument();
+
+    renderHistory.push('/favorite-recipes');
+    const favoriteRecipesLogo = screen.getByAltText(/favorite-recipes/i);
+    expect(favoriteRecipesLogo).toBeInTheDocument();
   });
 });
